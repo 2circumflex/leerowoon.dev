2e680c96d46ab0179a781457347daee849144e02
Roy <2circumflex@gmail.com>
2024-11-12 16:20:19 +0900

    Pretendard 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+
 import "./globals.css";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "개발 블로그",
@@ -18,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko" className="h-full scroll-my-20 scroll-smooth">
-      <body className={inter.className}>
+      <body className="font-pretendard">
         <div className="flex min-h-screen flex-col">
           <Header />
           <main className="mt-[64px] flex flex-1 flex-col">{children}</main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        pretendard: ["Pretendard"],
+      },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
