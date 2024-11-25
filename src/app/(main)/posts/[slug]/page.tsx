52cb8a771b18bf0106fdfba3cf885ecabba51598
Roy <2circumflex@gmail.com>
2024-11-25 22:40:25 +0900

    PostDetailPage에 metadata추가

--- src/app/(main)/posts/[slug]/page.tsx
+++ src/app/(main)/posts/[slug]/page.tsx
@@ -1,6 +1,39 @@
-import { getPost, getPostPaths, parsePostFileName } from "@/lib/post";
+import { Metadata } from "next";
+
 import PostHeader from "@/components/post-detail/post-header";
 import PostBody from "@/components/post-detail/post-body";
+import { getPost, getPostPaths, parsePostFileName } from "@/lib/post";
+import { baseUrl, siteName } from "@/lib/metadata";
+
+export async function generateMetadata({
+  params: { slug },
+}: PostDetailPageProps): Promise<Metadata> {
+  const post = await getPost(slug);
+  const title = `${post.title} | ${siteName}`;
+  const description = post.desc;
+  const imageURL = `${baseUrl}/${post.thumbnail}`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      locale: "ko_KR",
+      type: "article",
+      siteName: siteName,
+      publishedTime: post.date.toISOString(),
+      url: `${baseUrl}/posts/${post.slug}`,
+      images: [imageURL],
+    },
+    twitter: {
+      title,
+      description,
+      card: "summary_large_image",
+      images: [imageURL],
+    },
+  };
+}
 
 export const dynamicParams = false;
 
---
+++ src/lib/metadata.ts
@@ -0,0 +1,2 @@
+export const siteName = "lrow.dev";
+export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
