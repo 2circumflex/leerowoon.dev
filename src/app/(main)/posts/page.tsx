9fc7378f4aa66a3b2e019ae15f2bdcb293359214
Roy <2circumflex@gmail.com>
2024-11-25 23:44:49 +0900

    PostsPage ogimage 설정

--- src/app/(main)/posts/page.tsx
+++ src/app/(main)/posts/page.tsx
@@ -1,4 +1,37 @@
+import { Metadata } from "next";
+
 import PostList from "@/components/posts/post-list";
+import { siteName } from "@/lib/metadata";
+
+export const dynamicParams = false;
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  const title = `Posts | ${siteName}`;
+  return {
+    title,
+    openGraph: {
+      title,
+      type: "website",
+      images: [
+        {
+          url: "/og-image.png",
+          width: 1200,
+          height: 630,
+          alt: siteName,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      images: ["/og-image.png"],
+    },
+  };
+};
+
+export async function generateStaticParams() {
+  return [{}];
+}
 
 export default async function PostsPage() {
   return (
