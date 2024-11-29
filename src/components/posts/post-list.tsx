c2a9061c1170eae56bcecc7814f4f02ef6ba2221
Roy <2circumflex@gmail.com>
2024-11-29 16:11:20 +0900

    포스트 목록 디자인 변경

--- src/components/posts/post-list.tsx
+++ src/components/posts/post-list.tsx
@@ -9,7 +9,7 @@ export default async function PostList() {
   const postlist = await getDescSortedPostList();
 
   return (
-    <div className="mx-auto max-w-[1000px] grid grid-cols-1 md:grid-cols-2 gap-6">
+    <div className="flex flex-col gap-16">
       {postlist.map((post) => (
         <PostCard key={post.slug} post={post} />
       ))}
@@ -20,24 +20,28 @@ export default async function PostList() {
 const PostCard = ({ post }: { post: Post }) => {
   return (
     <Link key={post.slug} href={`/posts/${post.slug}`}>
-      <article className="p-4 w-full h-full flex flex-col rounded-lg border transition-colors group">
-        <FixedSizeImgWithPlaceholder
-          className="w-full h-full aspect-video object-cover rounded-lg"
-          src={post.thumbnail}
-          alt={post.title}
-          width={380}
-          height={214}
-          priority={false}
-        />
-        <h2 className="mt-3 text-2xl font-bold line-clamp-2 min-h-[3.5rem] leading-7 text-foreground group-hover:text-primary transition-colors duration-300">
-          {post.title}
-        </h2>
-        <p className="mt-3 text-muted-foreground line-clamp-2 min-h-[3rem] leading-6">
-          {post.desc}
-        </p>
-        <time className="mt-4 text-xs text-muted-foreground/60">
-          {dayjs(post.date).locale("ko").format("YYYY년 MM월 DD일")}
-        </time>
+      <article className="w-full flex flex-row justify-between gap-6 md:gap-10 transition-colors group">
+        <div className="flex flex-col flex-1">
+          <h2 className="text-xl md:text-2xl font-bold text-foreground group-hover:text-primary transition-colors duration-300">
+            {post.title}
+          </h2>
+          <p className="mt-2 text-sm text-muted-foreground line-clamp-3 leading-6">
+            {post.desc}
+          </p>
+          <time className="mt-4 text-xs text-muted-foreground/70">
+            {dayjs(post.date).locale("ko").format("YYYY년 MM월 DD일")}
+          </time>
+        </div>
+        <div className="w-[160px] md:w-[200px] shrink-0">
+          <FixedSizeImgWithPlaceholder
+            className="w-full aspect-video object-cover rounded-lg group-hover:scale-105 transition-transform duration-300 border dark:border-none"
+            src={post.thumbnail}
+            alt={post.title}
+            width={280}
+            height={158}
+            priority={false}
+          />
+        </div>
       </article>
     </Link>
   );
