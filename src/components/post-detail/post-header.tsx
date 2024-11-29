378596f5c3ccbf3693af7ebb75d178a696311efb
Roy <2circumflex@gmail.com>
2024-11-29 15:34:51 +0900

    포스트 상세 헤더 디자인 수정

--- src/components/post-detail/post-header.tsx
+++ src/components/post-detail/post-header.tsx
@@ -6,31 +6,31 @@ import { FixedSizeImgWithPlaceholder } from "@/components/fixed-size-img-with-pl
 
 export default function PostHeader({ post }: { post: Post }) {
   return (
-    <div className="flex flex-col items-center text-center">
-      <h1 className="text-3xl font-bold">{post.title}</h1>
+    <div className="flex flex-col">
+      <FixedSizeImgWithPlaceholder
+        className="mt-0 w-full aspect-video object-cover rounded-lg border dark:border-none"
+        src={post.thumbnail}
+        alt={post.title}
+        width={700}
+        height={395}
+      />
+      <h1 className="mb-4 text-4xl font-bold">{post.title}</h1>
       <div className="flex flex-row items-center gap-2">
         <FiCalendar className="size-4 text-gray-500" />
         <p className="text-sm text-gray-500">
           {dayjs(post.date).locale("ko").format("YYYY년 MM월 DD일")}
         </p>
       </div>
-      <div className="mt-4 flex flex-wrap justify-center items-center gap-2 gap-y-0">
+      <div className="mt-3 mb-0 flex flex-wrap gap-x-2 gap-y-0">
         {post.tags.map((tag) => (
           <p
             key={tag}
-            className="text-sm text-gray-500 bg-gray-100 dark:text-gray-400 dark:bg-gray-800 rounded-full px-2.5 py-1"
+            className="text-sm text-gray-500 bg-gray-100 dark:text-gray-400 dark:bg-gray-800 rounded-full px-2.5 py-2"
           >
-            {tag}
+            #{tag}
           </p>
         ))}
       </div>
-      <FixedSizeImgWithPlaceholder
-        className="mt-8 mb-0 w-full aspect-video object-cover rounded-lg border dark:border-none"
-        src={post.thumbnail}
-        alt={post.title}
-        width={700}
-        height={395}
-      />
     </div>
   );
 }
