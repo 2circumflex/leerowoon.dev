8da96ad673d9d7ee35dc5c22d0fd6924ea87ba95
Roy <2circumflex@gmail.com>
2024-12-31 20:03:02 +0900

    홈 페이지 기본 레이아웃

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,32 @@
+import { Metadata } from "next";
+
+import { baseUrl, siteMetadata, siteName } from "@/lib/metadata";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const url = `${baseUrl}`;
+
+  return {
+    title: `${siteName}`,
+    alternates: {
+      canonical: url,
+    },
+    openGraph: {
+      ...siteMetadata.openGraph,
+      title: `${siteName}`,
+      url,
+    },
+    twitter: {
+      ...siteMetadata.twitter,
+      title: `${siteName}`,
+    },
+  };
+}
+
+export default async function Home() {
+  return (
+    <div className="container mx-auto max-w-[900px] mt-32 mb-16 flex flex-col p-2 px-6">
+      {/* 홈 커버 */}
+      {/* 최근 포스트 */}
+    </div>
+  );
+}
